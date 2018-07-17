0abb32becfb5f3e6cedb15c9204f22e7b9e6a24f
Tao <41298349+ttaoREtw@users.noreply.github.com>
2018-07-17 13:01:20 +0800

    Update train_BiRNN.py

--- main/train_BiRNN.py
+++ main/train_BiRNN.py
@@ -19,14 +19,42 @@ import numpy as np
 import tensorflow as tf
 from utils.timit_dataset import TimitDataset, load_data
 from utils.timit_preprocess import idx2phn, phonemes
-from model.BiRNN import BiRNN, hps
+from model.BiRNN import BiRNN
+
+
+# parameter
+hps_list = {
+    'num_epochs': 70,
+    'lr': 5e-3,
+    'grad_clip': 5,
+    'num_hidden': 128,
+    'num_features': 39,
+    'num_classes': 61+1,
+    'num_cells': 2,
+    'num_layers': 1,
+    'batch_size': 32,
+    # 'max_time_step': 778,
+    'drop_prob': 0.2
+}
+
+# Can be accessed as 'hps.attr'
+def hparas(hps_list):
+    class Hparas(object):
+        pass
+    hps = Hparas()
+    for hp in hps_list:
+        setattr(hps, hp, hps_list[hp])
+    return hps
+
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: python3 train_BiRNN.py <timit_mfcc_path>')
         sys.exit(1)
-
+    # Create a hyperparater object
+    hps = hparas(hps_list)
+    
     feat_path = sys.argv[1]
     X_tr, y_tr, X_val, y_val, X_te, y_te = load_data(feat_path)
 
