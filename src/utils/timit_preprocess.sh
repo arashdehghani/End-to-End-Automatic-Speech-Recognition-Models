49d7691bdcdea227d37623dccf8bfd4c5f966dc2
ttaoREtw <ttaoREtw@gmail.com>
2018-07-29 00:58:59 +0800

    update timit_preprocess.sh

--- src/utils/timit_preprocess.sh
+++ src/utils/timit_preprocess.sh
@@ -1,7 +1,7 @@
 # Reference: https://github.com/XenderLiu/Listen-Attend-and-Spell-Pytorch/blob/master/util/timit_preprocess.sh
 
 if [ $# -ne 2 ]; then
-    echo "Usage: ./timit_preprocess.sh <timit_directory> <output_fileName>"
+    echo "Usage: timit_preprocess.sh <timit_directory> <output_fileName>"
     exit 1
 fi
 
@@ -11,4 +11,4 @@ echo ' '
 # Turn NIST wav file into RIFF
 # find $1 -name '*.wav' | parallel -P20 sox {} '{.}_riff.wav'
 # Extract mfcc features
-python3 ./utils/timit_preprocess.py $1 $2
+python3 ./src/utils/timit_preprocess.py $1 $2
